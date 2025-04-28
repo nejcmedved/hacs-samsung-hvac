49c6358821eb17378a198956d82dc1322117cb79
Nejc Medved <nejc.medved@asist.si>
2025-04-28 22:02:03 +0200

    - logging update

---
+++ custom_components/hacs_samsung_hvac/heat_pump.py
@@ -0,0 +1,20 @@
+import threading
+import time
+
+_LOGGER = logging.getLogger(__name__)
+
+class HeatPump(threading.Thread):
+    def __init__(self, ip: str, port:int):
+        super().__init__()
+        self.ip = ip
+        self.port = port
+        self._is_running = False
+        self.daemon = True
+        self.start()    
+            
+    def run(self):
+        self._is_running = True
+        # Simulate some work being done in the thread
+        while self._is_running:
+            _LOGGER.info(f"HeatPump running on {self.ip}:{self.port}")
+            time.sleep(1)
