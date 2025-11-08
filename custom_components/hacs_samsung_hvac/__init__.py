db59e2eea8a17e3861089aebbb48ab81a92f8b41
Nejc Medved <nejc.medved@asist.si>
2025-11-08 13:56:05 +0100

    - fix migration

--- custom_components/hacs_samsung_hvac/__init__.py
+++ custom_components/hacs_samsung_hvac/__init__.py
@@ -57,7 +57,7 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> b
     # This calls the async_setup method in each of your entity type files.
     for platform in PLATFORMS:
         hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(config_entry, platform)
+            hass.config_entries.async_forward_entry_setups(config_entry, [platform])
         )
 
     # Return true to denote a successful setup.
